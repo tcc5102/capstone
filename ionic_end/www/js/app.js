f38adcfef9aeab801c7b7c73b27eb0f7023837ae
Tyler Cooper <tyler.cooper91@gmail.com>
2017-01-27 13:01:18 -0500

    menu started, click to open

--- ionic_end/www/js/app.js
+++ ionic_end/www/js/app.js
@@ -3,6 +3,8 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
+angular.module('todo', ['ionic'])
+
 angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
@@ -22,3 +24,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
 })
+
+// angular.module('test', ['ionic'])
